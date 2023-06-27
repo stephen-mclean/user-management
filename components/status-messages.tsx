965b0732c8d3222e40887bb7c17702c4cd179432
Stephen McLean <stephenmclean@MacBook-Air.local>
2023-06-27 19:41:21 +0100

    todos

--- components/status-messages.tsx
+++ components/status-messages.tsx
@@ -5,6 +5,8 @@ import Alert from "./alert";
 
 export default function StatusMessages() {
   const searchParams = useSearchParams();
+  // TODO: store the param keys in a constant
+  // and use it where we add the params to the URL as well
   const added = searchParams?.get("added");
   const edited = searchParams?.get("edited");
   const deleted = searchParams?.get("deleted");
